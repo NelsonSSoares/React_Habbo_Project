6fb31828d7bfec1b27b27ea61890fc8df343e413
Nelson <punkpunkada@gmail.com>
2026-03-26 22:09:42 -0300

    testing hook by id

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 import { useHabboByName } from "./hooks/useHabboByName";
+import { useHabboById } from "./hooks/useHabboById";
 function App() {
   // ✅ direto no componente, sem useEffect, sem async
   const { data: user, loading, error } = useHabboByName("amigo-punk");
+  const { data: user2 } = useHabboById(user?.uniqueId || "");
 
   useEffect(() => {
     console.log(user);
   }, [user]);
+  useEffect(() => {
+  console.log("User 2:", user2);
+  }, [user2]);
 
   if (loading) return <p>Carregando...</p>;
   if (error)   return <p>Erro: {error}</p>;
